88a17fe0838e7cb22ebd2f319a161eb8b2b7bf48
Alexys <alexysavenda110406@gmail.com>
2025-05-31 10:49:20 -0600

    Principal_Code_Modify

--- Game/main.py
+++ Game/main.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
-import math
-import random
+from Pesonaje import * 
+from Asteroid import * 
 
 # Inicialización de Pygame
 pygame.init()
@@ -16,101 +16,6 @@ pygame.display.set_caption("Asteroids")
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 
-class Personaje(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        try:
-            self.image = pygame.image.load("Image/player-01.png")
-            self.image = pygame.transform.scale(self.image, (50, 50))
-        except:
-            # Si no se encuentra la imagen, crear un triángulo como nave
-            self.image = pygame.Surface((50, 50), pygame.SRCALPHA)
-            pygame.draw.polygon(self.image, BLANCO, [(25, 0), (0, 50), (50, 50)])
-        
-        self.rect = self.image.get_rect()
-        self.rect.center = (ANCHO // 2, ALTO // 2)
-        self.velocidad = 0
-        self.angulo = 0
-        self.aceleracion = 0.1
-        self.rotacion_velocidad = 5
-        self.friccion = 0.98
-
-    def update(self):
-        # Rotación
-        teclas = pygame.key.get_pressed()
-        if teclas[pygame.K_LEFT]:
-            self.angulo += self.rotacion_velocidad
-        if teclas[pygame.K_RIGHT]:
-            self.angulo -= self.rotacion_velocidad
-        
-        # Aceleración
-        if teclas[pygame.K_UP]:
-            self.velocidad += self.aceleracion
-        
-        # Actualizar posición basada en velocidad y ángulo
-        self.rect.x += math.cos(math.radians(self.angulo)) * self.velocidad
-        self.rect.y -= math.sin(math.radians(self.angulo)) * self.velocidad
-        
-        # Aplicar fricción
-        self.velocidad *= self.friccion
-        
-        # Rotar la imagen
-        self.image = pygame.transform.rotate(self.image, self.angulo)
-        self.rect = self.image.get_rect(center=self.rect.center)
-        
-        # Mantener al personaje dentro de la pantalla
-        if self.rect.left > ANCHO:
-            self.rect.right = 0
-        elif self.rect.right < 0:
-            self.rect.left = ANCHO
-        if self.rect.top > ALTO:
-            self.rect.bottom = 0
-        elif self.rect.bottom < 0:
-            self.rect.top = ALTO
-
-class Asteroide(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((40, 40), pygame.SRCALPHA)
-        pygame.draw.polygon(self.image, BLANCO, [
-            (20, 0), (40, 20), (20, 40), (0, 20)
-        ])
-        self.rect = self.image.get_rect()
-        self.velocidad_x = random.uniform(-2, 2)
-        self.velocidad_y = random.uniform(-2, 2)
-        self.rotacion = random.uniform(-3, 3)
-        self.angulo = 0
-        
-        # Posición inicial aleatoria en los bordes
-        lado = random.choice(['arriba', 'abajo', 'izquierda', 'derecha'])
-        if lado == 'arriba':
-            self.rect.x = random.randint(0, ANCHO)
-            self.rect.y = -40
-        elif lado == 'abajo':
-            self.rect.x = random.randint(0, ANCHO)
-            self.rect.y = ALTO
-        elif lado == 'izquierda':
-            self.rect.x = -40
-            self.rect.y = random.randint(0, ALTO)
-        else:
-            self.rect.x = ANCHO
-            self.rect.y = random.randint(0, ALTO)
-
-    def update(self):
-        self.rect.x += self.velocidad_x
-        self.rect.y += self.velocidad_y
-        self.angulo += self.rotacion
-        self.image = pygame.transform.rotate(self.image, self.rotacion)
-        
-        # Mantener los asteroides dentro de la pantalla
-        if self.rect.left > ANCHO:
-            self.rect.right = 0
-        elif self.rect.right < 0:
-            self.rect.left = ANCHO
-        if self.rect.top > ALTO:
-            self.rect.bottom = 0
-        elif self.rect.bottom < 0:
-            self.rect.top = ALTO
 
 # Crear sprites
 personaje = Personaje()
